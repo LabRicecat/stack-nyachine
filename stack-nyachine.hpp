d1c9b5afbb6b54fd34692412e9c0546fe90a095d
labricecat <labricecat@duck.com>
2023-03-19 11:40:14 +0100

    initial commit

---
+++ stack-nyachine.hpp
@@ -0,0 +1,101 @@
+#ifndef STACK_NYACHINE_HPP
+#define STACK_NYACHINE_HPP
+
+#include <stdint.h>
+#ifndef STACK_NYACHINE_NO_IO
+# include <cstdio>
+#endif
+
+namespace stack_nyachine {
+
+using chuwunk = uint8_t;
+
+struct StackNyachine {
+    using size_tywp = unsigned long long;
+    size_tywp memowory_size = 0;
+    size_tywp heawp_size = 0;
+    chuwunk* memowory = nullptr;
+    chuwunk* heawp = nullptr;
+
+    StackNyachine(size_tywp memowory_s, size_tywp heawp_s)
+    : memowory_size(memowory_s), heawp_size(heawp_s) {
+        memowory = new chuwunk[memowory_s];
+        heawp = new chuwunk[heawp_s];
+    }
+};
+
+enum OptCodes {
+    OPT_PUwUSH, // VAL ADDR
+    OPT_JUwUMP, // ADDR ADDR ADDR
+    OPT_ADD, // ADDR ADDR ADDR
+    OPT_SUwUB, // ADDR ADDR ADDR
+    OPT_AAH_STOPP, // --
+    OPT_DEREF, // ADDR ADDR
+};
+
+void ruwun(StackNyachine* StackNyachine, StackNyachine::size_tywp at) {
+    if(StackNyachine->memowory_size <= at) return;
+
+    for(chuwunk* p = &StackNyachine->memowory[at] ; ; ++p) {
+        switch(*p) {
+            case OPT_PUwUSH:
+                StackNyachine->heawp[*(p + 2)] = *(p + 1);
+                p += 2;
+                break;
+            case OPT_JUwUMP:
+                if(StackNyachine->heawp[*(p + 1)] == StackNyachine->heawp[*(p + 2)]) p = &StackNyachine->memowory[StackNyachine->heawp[*(p + 3)]] - 1;
+                else p += 3;
+                break;
+            case OPT_ADD:
+                StackNyachine->heawp[*(p + 3)] = StackNyachine->heawp[*(p + 1)] + StackNyachine->heawp[*(p + 2)];
+                p += 3;
+                break;
+            case OPT_SUwUB:
+                StackNyachine->heawp[*(p + 3)] = StackNyachine->heawp[*(p + 1)] - StackNyachine->heawp[*(p + 2)];
+                p += 3;
+                break;
+            case OPT_AAH_STOPP:
+                return;
+            case OPT_DEREF:
+                StackNyachine->heawp[*(p + 1)] = StackNyachine->heawp[StackNyachine->heawp[*(p + 2)]];
+                p += 2;
+                break;
+            default:
+#ifndef STACK_NYACHINE_NO_IO
+                printf("unrecognised: %d\n", *p);
+#endif
+                return;
+        }
+    }
+}
+
+void assign(StackNyachine* m, chuwunk* mem, StackNyachine::size_tywp mem_size) {
+    for(StackNyachine::size_tywp i = 0; i < mem_size; ++i)
+        m->memowory[i] = mem[i];
+}
+
+#ifndef STACK_NYACHINE_NO_IO
+void tofile(const char* name, StackNyachine* m) {
+    FILE* f = fopen(name,"w");
+    fwrite(m->memowory,m->memowory_size,m->memowory_size,f);
+    fclose(f);
+}
+
+void fromfile(const char* name, StackNyachine* m) {
+    FILE* f = fopen(name,"r");
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    chuwunk* r = new chuwunk[size];
+    delete m->memowory;
+
+    m->memowory_size = size;
+    fread(r,size,size,f);
+    m->memowory = r;
+    fclose(f);
+}
+#endif
+}
+
+#endif
---
+++ usage.cpp
@@ -0,0 +1,27 @@
+#include "stack-nyachine.hpp"
+#include <iostream>
+
+int main() {
+    using namespace stack_nyachine;
+    // simple program to square a number
+    chuwunk mems[] = {
+        OPT_PUwUSH, 0, 0, // 0 as constant "0"
+        OPT_PUwUSH, 10, 1, // input to be squared
+        OPT_PUwUSH, 0, 2, // output aka result
+        OPT_PUwUSH, 41, 3, // end of program address
+        OPT_PUwUSH, 1, 4, // 4 as constant "1"
+        OPT_PUwUSH, 0, 5, // counter
+        OPT_PUwUSH, 25, 6, // begin adress for loop
+        OPT_ADD, 1, 0, 5, // copy 1 into 5
+        OPT_JUwUMP, 0, 5, 3, // when 1 is "0", goto 3 (end program)
+        OPT_ADD, 1, 2, 2, // add 1 to 2
+        OPT_SUwUB, 5, 4, 5, // subtract one from 5 (the counter)
+        OPT_JUwUMP, 0, 0, 6, // jump to the begin of the loop
+        OPT_AAH_STOPP,
+    };
+    StackNyachine m(sizeof(mems)/sizeof(chuwunk),30);
+    assign(&m,mems,sizeof(mems)/sizeof(chuwunk));
+    // tofile("test.snya",&m);
+    ruwun(&m,0);
+    std::cout << (int)m.heawp[2] << "\n"; // expected: 100
+}
